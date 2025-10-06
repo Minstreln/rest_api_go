aee9bba8f563f9e8502a7b60cfb05d8f2efffa93
Minstrel Nwachukwu <minstreln@gmail.com>
2025-10-06 19:34:09 +0100

    All endpoints done and ready

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -2,54 +2,95 @@ package main
 
 import (
 	"crypto/tls"
+	"embed"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	mw "restapi/internal/api/middlewares"
 	"restapi/internal/api/routers"
-	"restapi/internal/repositories/sqlconnect"
+	"restapi/pkg/utils"
+	"time"
 
 	"github.com/joho/godotenv"
 )
 
-func main() {
-	err := godotenv.Load()
+//go:embed .env
+var envFile embed.FS
+
+func loadEnvFromEmbeddedFile() {
+	// read the embedded .env
+	content, err := envFile.ReadFile(".env")
 	if err != nil {
-		return
+		log.Fatalf("Error reading from .env file: %v", err)
 	}
 
-	_, err = sqlconnect.ConnectDb()
+	// create a temp file to load the env variables
+	tempFile, err := os.CreateTemp("", ".env")
 	if err != nil {
-		fmt.Println("Error-----:", err)
-		return
+		log.Fatalf("Error creating temp .env file: %v", err)
 	}
+	defer os.Remove(tempFile.Name())
+
+	// write content in the temp file
+	_, err = tempFile.Write(content)
+	if err != nil {
+		log.Fatalf("Error writting to temp .env file: %v", err)
+	}
+
+	err = tempFile.Close()
+	if err != nil {
+		log.Fatalf("Error closing temp .env file: %v", err)
+	}
+
+	// load env variables from temp file
+	err = godotenv.Load(tempFile.Name())
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+}
+
+func main() {
+	// only in production for running source code
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	return
+	// }
+
+	// load environment variables from the embedded .env
+	loadEnvFromEmbeddedFile()
+
+	fmt.Println("environment variable CERT_FILE", os.Getenv("CERT_FILE"))
 
 	port := os.Getenv("SERVER_PORT")
 
-	cert := "cert.pem"
-	key := "key.pem"
+	// cert := "cert.pem"
+	// key := "key.pem"
+
+	cert := os.Getenv("CERT_FILE")
+	key := os.Getenv("KEY_FILE")
 
 	tlsConfig := &tls.Config{
-		MinVersion: tls.VersionTLS12,
+		MinVersion: tls.VersionTLS10,
 	}
 
-	// rl := mw.NewRateLimiter(5, time.Minute)
+	rl := mw.NewRateLimiter(5, time.Minute)
 
-	// hppOptions := mw.HPPOptions{
-	// 	CheckQuery:                  true,
-	// 	CheckBody:                   true,
-	// 	CheckBodyOnlyForContentType: "application/x-www-form-urlencoded",
-	// 	Whitelist:                   []string{"sortBy", "sortOrder", "name", "age", "class"},
-	// }
+	hppOptions := mw.HPPOptions{
+		CheckQuery:                  true,
+		CheckBody:                   true,
+		CheckBodyOnlyForContentType: "application/x-www-form-urlencoded",
+		Whitelist:                   []string{"sortBy", "sortOrder", "name", "age", "class"},
+	}
 
 	// secureMux := mw.Hpp(hppOptions)(rl.Middleware(mw.Compression(mw.ResponseTimeMiddleware(mw.SecurityHeaders(mw.Cors(mux))))))
+	// secureMux := jwtMiddleware(mw.SecurityHeaders(router))
+	// secureMux := mw.SecurityHeaders(router)
 
-	// secureMux := utils.ApplyMiddlewares(mux, mw.Hpp(hppOptions), mw.Compression, mw.SecurityHeaders, mw.ResponseTimeMiddleware, rl.Middleware, mw.Cors)
 	router := routers.MainRouter()
 	jwtMiddleware := mw.MiddlewaresExcludePaths(mw.JWTMiddleware, "/execs/login", "/execs/forgotpassword", "/execs/resetpassword/reset")
-	secureMux := jwtMiddleware(mw.SecurityHeaders(router))
-	// secureMux := mw.SecurityHeaders(router)
+
+	secureMux := utils.ApplyMiddlewares(router, mw.SecurityHeaders, mw.Compression, mw.Hpp(hppOptions), jwtMiddleware, mw.ResponseTimeMiddleware, rl.Middleware, mw.Cors)
 
 	// create custom server
 	server := &http.Server{
@@ -59,7 +100,7 @@ func main() {
 	}
 
 	fmt.Println("Server is running on port", port)
-	err = server.ListenAndServeTLS(cert, key)
+	err := server.ListenAndServeTLS(cert, key)
 	if err != nil {
 		log.Fatalln("Error starting the server", err)
 	}
